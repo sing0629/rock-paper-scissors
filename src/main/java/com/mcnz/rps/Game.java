076f3e6de96f71e913313b364653cbcbcb9d62bc
sing0629 <58870660+sing0629@users.noreply.github.com>
2023-01-31 21:23:54 +0800

    Update Game.java

--- src/main/java/com/mcnz/rps/Game.java
+++ src/main/java/com/mcnz/rps/Game.java
@@ -7,7 +7,7 @@ public class Game {
 	public static void main(String[] args) {
 		String prompt = "Will it be rock, paper or scissors?";
 		String input = JOptionPane.showInputDialog(prompt);
-		System.out.println("You selected: " + input);
+		System.out.println("test You selected: " + input);
 	}
 
 }
